7e177d732d2bf8c5dbda9013cd7908e6cf562337
hari-testpress <hari@testpress.in>
2021-09-03 18:01:03 +0530

    Refactor test case

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -7,17 +7,16 @@ import datetime
 
 
 class TestQuestionModel(TestCase):
-    def test_was_published_recently_with_future_question(self):
-        time = timezone.now() + datetime.timedelta(days=30)
-        future_question = Question(question_text="whats up", pub_date=time)
-        self.assertFalse(future_question.was_published_recently)
-
-    def test_was_published_recently_with_old_question(self):
-        time = timezone.now() - datetime.timedelta(days=1, seconds=1)
+    def test_was_published_recently_should_return_false_for_question_older_than_a_day(
+        self,
+    ):
+        time = timezone.now() - datetime.timedelta(days=2)
         old_question = Question(pub_date=time)
         self.assertFalse(old_question.was_published_recently)
 
-    def test_was_published_recently_with_present_question(self):
+    def test_was_published_recently_should_return_true_for_question_created_within_day(
+        self,
+    ):
         present_question = Question(
             question_text="2+2", pub_date=timezone.now()
         )
