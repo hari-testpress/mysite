1e05e48ec68e7efe5f225228497de7da21cf0674
hari-testpress <89839073+hari-testpress@users.noreply.github.com>
2021-09-03 18:06:07 +0530

    Add was_published_recently method (#10)
    - This method returns true for question created within a day.

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
+from .models import Question
+from django.utils import timezone
+import datetime
 
 # Create your tests here.
+
+
+class TestQuestionModel(TestCase):
+    def test_was_published_recently_should_return_false_for_question_older_than_a_day(
+        self,
+    ):
+        time = timezone.now() - datetime.timedelta(days=2)
+        old_question = Question(pub_date=time)
+        self.assertFalse(old_question.was_published_recently)
+
+    def test_was_published_recently_should_return_true_for_question_created_within_day(
+        self,
+    ):
+        present_question = Question(
+            question_text="2+2", pub_date=timezone.now()
+        )
+        self.assertTrue(present_question.was_published_recently)
